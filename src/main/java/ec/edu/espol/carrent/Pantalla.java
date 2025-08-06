3f93b4de1bc60e94449c08faa6104b7df9f89bf4
IvanAlvarado999 <ivdaalca55@gmail.com>
2025-08-06 18:44:37 -0500

    Add files via upload

--- src/main/java/ec/edu/espol/carrent/Pantalla.java
+++ src/main/java/ec/edu/espol/carrent/Pantalla.java
@@ -102,6 +102,7 @@ public class Pantalla {
                         System.out.println("10. Eliminar registro de la tabla Servicio");
                         System.out.println("11. Eliminar registro de la tabla Modificacion");
                         System.out.println("12. Eliminar registro de la tabla Mantenimiento");
+                        System.out.println("13. Salir");
                         
                         subOpcion = sc.nextInt();
                         sc.nextLine();
@@ -170,7 +171,7 @@ public class Pantalla {
                                 System.out.println("Opcion incorrecta");
                             
                         }
-                    }while (subOpcion != 12);
+                    }while (subOpcion != 13);
                 break;
         }
     }
